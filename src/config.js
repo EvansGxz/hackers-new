377b778999e638254514fa2025b436acf2d48e40
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-08-25 19:52:30 -0500

    Add uris

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const ANGULAR_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=angular&page=0";
+export const REACT_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0";
+export const VUEJS_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=vuejs&page=0";
