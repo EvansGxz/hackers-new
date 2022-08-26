27746b6f0c561d8204620c64dcb6a6cb9fda85bb
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-08-25 19:40:09 -0500

    Add typography

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,9 +1,15 @@
 export const colors = {
   gray: {
-    light: "#E0E0E0",
-    medium: "#666666",
-    dark: "#212121",
+    light: "#767676",
+    medium: "#6b6b6b",
+    high: "#606060",
+  },
+  black: {
+    light: "#3B3B3B",
+    medium:"#343434",
+    high: "#000"
   },
   white: "#FFF",
-  background: "#FEFEFE",
+  background: "#FCFCFC",
+  blue: "#1890ff",
 };
