ef286895c4659ebcf10f027da886417feb9d7bee
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-08-25 20:15:03 -0500

    Add index route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
-
+import styled from "@emotion/styled";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { colors } from "./styles";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.background};
+  margin: 8rem 15rem;
+  justify-content: space-between;
+  align-content: center;
+  height: 100vh;
+`;
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <Container>
+      <Routes>
+        <>
+          <Route index element={<Navigate to="home" />} />
+          <Route path="/home" element={<h1>Index</h1>} />
+        </>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
