51fd873eda79ed7325504f329630143dd77959a6
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-08-26 20:23:37 -0500

    Add option menu

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,10 @@
 import styled from "@emotion/styled";
 import { CardItem } from "../components/cards";
 import icontimer from "../assets/icontimer.png";
-import Favorite from "../assets/heart_on.png";
+//import Favorite from "../assets/heart_on.png";
 import Unfavorite from "../assets/heart_off.png"
 import { colors, fonts, typography } from "../styles";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,6 +12,15 @@ const Wrapper = styled.div`
   height: 15vh;
   align-items: center;
   box-shadow: 0 1px 4px 0 rgba(0, 21, 41, 0.12);
+`;
+
+const Options = styled.span`
+  ${typography.md}
+  font-weight: 500;
+  cursor: pointer;
+  border-radius: 2px;
+  padding: 0.188rem 1rem;
+  margin: 4.375rem 0;
 `;
 
 const Tittle = styled.span`
@@ -22,10 +32,9 @@ const Tittle = styled.span`
 `;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+
   background-color: ${colors.background};
-  margin: 4.375rem 9.375rem;
+  margin: 0 9.375rem;
   justify-content: space-between;
   align-content: center;
   height: 85vh;
@@ -41,10 +50,32 @@ const Header = () => {
 };
 
 export default function HomePage() {
+  const [showLogin, setShowLogin] = useState(true);
   return (
     <>
       <Header />
       <Container>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+            <Options
+              onClick={() => setShowLogin(true)}
+              style={
+                showLogin ? { border: `solid 1px ${colors.blue}` } : {border: `solid 1px #d6d6d6` }
+              }
+            >
+              All
+            </Options>
+            <Options
+              onClick={() => setShowLogin(false)}
+              style={
+                !showLogin
+                  ? { border: `solid 1px ${colors.blue}` }
+                  : {border: `solid 1px #d6d6d6` }
+              }
+            >
+              My faves
+            </Options>
+          </div>
+          {/*showLogin ? <LoginForm /> : <SignupForm />*/}
         <CardItem 
           timelogo={icontimer}
           tittle="3 hours ago  by author"
