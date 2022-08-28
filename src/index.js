609ba0b11de53d5bc68273f5eb138817e8e9e289
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-08-28 11:28:04 -0500

    Change render to root

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import { Global } from "@emotion/react";
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { global, reset } from "./styles";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Global styles={reset} />
     <Global styles={global} />
     <BrowserRouter>
         <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
