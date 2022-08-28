f874284d08b4ada6c7528ae8dba5a7052b09df38
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-08-27 19:44:13 -0500

    Remove default page

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const ANGULAR_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=angular&page=0";
-export const REACT_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0";
-export const VUEJS_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=vuejs&page=0";
+export const ANGULAR_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=angular&page=";
+export const REACT_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=";
+export const VUEJS_BASE_URI = "https://hn.algolia.com/api/v1/search_by_date?query=vuejs&page=";
