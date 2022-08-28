4a82d3b55969cf57d169a652b9aee1d2f16a0217
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-08-27 19:43:02 -0500

    Finish pagination

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,9 +2,12 @@ import styled from "@emotion/styled";
 import { CardItem } from "../components/cards";
 import icontimer from "../assets/icontimer.png";
 //import Favorite from "../assets/heart_on.png";
-import Unfavorite from "../assets/heart_off.png"
+import Unfavorite from "../assets/heart_off.png";
 import { colors, fonts, typography } from "../styles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { get } from "axios";
+import { REACT_BASE_URI } from "../config";
+import { Pagination } from "@mui/material";
 
 const Wrapper = styled.div`
   display: flex;
@@ -16,13 +19,14 @@ const Wrapper = styled.div`
 
 const Options = styled.span`
   ${typography.md}
-  font-weight: 500;
+  font-weight: bold;
   cursor: pointer;
   color: ${colors.gray.dark};
-  font-weight: bold;
   border-radius: 2px;
   padding: 0.188rem 1rem;
   margin: 4.375rem 0;
+  width: 6.125rem;
+  text-align: center;
 `;
 
 const Tittle = styled.span`
@@ -40,6 +44,13 @@ const Container = styled.div`
   height: 85vh;
 `;
 
+const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: 36.875rem 36.875rem;
+  justify-content: center;
+  max-height: 55vh;
+  overflow-y: scroll;
+`;
 
 const Header = () => {
   return (
@@ -50,39 +61,112 @@ const Header = () => {
 };
 
 export default function HomePage() {
-  const [showLogin, setShowLogin] = useState(true);
+  const [showAll, setShowAll] = useState(true);
+  const [page, setPage] = useState(1);
+
+
+  const handlePagination=(e, value)=>{
+    setPage(value);
+  }
   return (
     <>
       <Header />
       <Container>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-            <Options
-              onClick={() => setShowLogin(true)}
-              style={
-                showLogin ? { border: `solid 1px ${colors.blue}` } : {border: `solid 1px #d6d6d6` }
-              }
-            >
-              All
-            </Options>
-            <Options
-              onClick={() => setShowLogin(false)}
-              style={
-                !showLogin
-                  ? { border: `solid 1px ${colors.blue}` }
-                  : {border: `solid 1px #d6d6d6` }
-              }
-            >
-              My faves
-            </Options>
-          </div>
-          {/*showLogin ? <LoginForm /> : <SignupForm />*/}
-        <CardItem 
-          timelogo={icontimer}
-          tittle="2 hours ago  by author"
-          body="Aute tempor reprehenderit ex laboris. asdlkdadklasjdklajdakljdklajklfjaklsjdklajkdaklfjaklfjaklfaklflakjdklad.  Aliqua eiusmod est Lorem est fugiat reprehenderit eiusmod qui minim irure voluptate enim aliquip."
-          favorite={Unfavorite}
-        />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Options
+            onClick={() => setShowAll(true)}
+            style={
+              showAll
+                ? {
+                    border: `solid 1px ${colors.blue}`,
+                    color: `${colors.blue}`,
+                  }
+                : { border: `solid 1px #d6d6d6` }
+            }
+          >
+            All
+          </Options>
+          <Options
+            onClick={() => setShowAll(false)}
+            style={
+              !showAll
+                ? {
+                    border: `solid 1px ${colors.blue}`,
+                    color: `${colors.blue}`,
+                  }
+                : { border: `solid 1px #d6d6d6` }
+            }
+          >
+            My faves
+          </Options>
+        </div>
+        {showAll ? <All page={page}/> : <MyFaves />}
+        <div style={{display: 'flex', justifyContent: 'center', margin: '2rem 0'}}>
+
+        <Pagination count={10} page={page} onChange={handlePagination} variant="outline" shape="rounded" />
+        </div>
       </Container>
     </>
   );
+}
+
+const All = ({page}) => {
+  useEffect(() => {
+    get(REACT_BASE_URI + page).then((data) => setReactData(data.data.hits));
+  }, [page]);
+  const [reactData, setReactData] = useState(null);
+
+  return (
+    <CardContainer>
+      {reactData ? (
+        reactData.map((data) => (
+          <>
+            <CardItem
+              timelogo={icontimer}
+              tittle={`${timeSince(
+                new Date(
+                  reactData[0].created_at.split("T").join(" ").slice(0, -5)
+                )
+              )} ago by ${data.author}`}
+              body={data.comment_text}
+              favorite={Unfavorite}
+            />
+          </>
+        ))
+      ) : (
+        <p>No data found</p>
+      )}
+    </CardContainer>
+  );
+};
+
+const MyFaves = () => {
+  return <></>;
+};
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
 }
